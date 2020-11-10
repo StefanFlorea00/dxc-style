d179cb39078bc1ba0f41a5bb0d907c0e1ef98a05
Stefan <stefflr@yahoo.com>
2020-11-10 12:16:01 +0100

    updated form

--- script.js
+++ script.js
@@ -58,3 +58,22 @@ function userSendMessage(message) {
   document.querySelector(".chat-text").appendChild(line);
   document.querySelector(".chat-text").scrollTop = document.querySelector(".chat-text").scrollHeight;
 }
+
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};

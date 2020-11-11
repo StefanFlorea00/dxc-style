37b81f7becac2dfb3c060697686e9dd8d1f3b454
Stefan <stefflr@yahoo.com>
2020-11-11 09:40:46 +0100

    added localstorage

--- script-form.js
+++ script-form.js
@@ -1,81 +1,83 @@
+const formCompleted = localStorage.getItem("formComplete");
+console.log(formCompleted);
+
+//DB
+
 const key = "5faad2ec863959728838520e";
 const endpoint = "https://newdxc-e2c4.restdb.io/rest/contactdata";
 
 _GET();
 
-//DB
-
 function _GET() {
-    fetch(endpoint, {
-        method: "get",
-        headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-apikey": key,
-        "cache-control": "no-cache",
-        },
-    })
-        .then((e) => e.json())
-        .then(showData)
-        .catch(function (error) {
-        console.log("Request failed", error);
+  fetch(endpoint, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": key,
+      "cache-control": "no-cache",
+    },
+  })
+    .then((e) => e.json())
+    .then(showData)
+    .catch(function (error) {
+      console.log("Request failed", error);
     });
 }
 
-
 function _POST(data) {
-    const postData = JSON.stringify(data);
-    fetch(endpoint, {
-        method: "post",
-        headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-apikey": key,
-        "cache-control": "no-cache",
-        },
-        body: postData,
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            _GET(data);
+  const postData = JSON.stringify(data);
+  fetch(endpoint, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "x-apikey": key,
+      "cache-control": "no-cache",
+    },
+    body: postData,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      _GET(data);
     });
 }
 
 function showData(list) {
-    console.log(list);
+  console.log(list);
 }
 
 // FORM
 
 const form = document.querySelector("form");
 
-
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if(form.elements.terms.value == "on") {
-        form.elements.terms.value = true;
-    }
+  if (form.elements.terms.value == "on") {
+    form.elements.terms.value = true;
+  }
 
-    const formData = {
-        first_name: form.elements.first_name.value,
-        last_name: form.elements.last_name.value,
-        tel: form.elements.tel.value,
-        email: form.elements.email.value,
-        company: form.elements.company.value,
-        job: form.elements.job.value,
-        country: form.elements.country.value,
-        extra_info: form.elements.extra_info.value,
-        terms: form.elements.terms.value
-    };
+  const formData = {
+    first_name: form.elements.first_name.value,
+    last_name: form.elements.last_name.value,
+    tel: form.elements.tel.value,
+    email: form.elements.email.value,
+    company: form.elements.company.value,
+    job: form.elements.job.value,
+    country: form.elements.country.value,
+    extra_info: form.elements.extra_info.value,
+    terms: form.elements.terms.value,
+  };
 
-    console.log(form.elements.terms.value);
-    console.log(formData);
-    _POST(formData);
+  console.log(form.elements.terms.value);
+  console.log(formData);
+  _POST(formData);
 
-    document.querySelectorAll(".modal input").forEach((el) => (el.value = ""));
-    closeModal();
+  document.querySelectorAll(".modal input").forEach((el) => (el.value = ""));
+  document.querySelector(".modal input[type=submit]").value = "Submit";
+  closeModal();
+
+  localStorage.setItem("formComplete", "true");
 });
-
-
 
 // MODAL
 
